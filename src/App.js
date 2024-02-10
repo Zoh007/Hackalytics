770385fb1dbeb296d7eade76d644409c0998e2d9
Zohair Hussain <iqbalzohair03@gmail.com>
2024-02-10 18:34:42 -0500

    Add style

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import WeightEntry from './weightEntry.js';
 import myDataset from './datasets/myDataset.csv';
 import './App.css';
 
-
-
 const App = () => {
   return (
-    <div>
-      {/* Other components */}
-      <WeightEntry />
+    <div className="app-container">
+      <header className="app-header">
+        <h1>IgniteFit</h1>
+        <p>Unleash the Power Within You</p>
+      </header>
+
+      <div className="content-container">
+        {/* Other components */}
+        <WeightEntry />
+      </div>
+
+      <footer className="app-footer">
+        <p>Keep the Flame Burning! 🔥</p>
+      </footer>
     </div>
   );
 };
 
 export default App;
-
